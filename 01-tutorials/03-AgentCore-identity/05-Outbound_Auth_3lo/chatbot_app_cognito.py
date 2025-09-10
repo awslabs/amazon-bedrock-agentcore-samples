069804d22783eb5f9a217544dcabf8687a462e45
HT <hardik.thakkar00@gmail.com>
2025-09-10 04:25:49 +0000

    Updates to fix the Streamlit app access when running in sagemaker
    Modified
    - Requirements.txt (added dependencies)
    - chatbot_app_cognito.py (added get_streamlit_url, for sagemaker access)
    - runtime_with_strands_and_egress_3lo.ipynb (streamlit piece for access url, cosmetic updates)

--- 01-tutorials/03-AgentCore-identity/05-Outbound_Auth_3lo/chatbot_app_cognito.py
+++ 01-tutorials/03-AgentCore-identity/05-Outbound_Auth_3lo/chatbot_app_cognito.py
@@ -6,12 +6,51 @@ import urllib.parse
 import logging
 import re
 from runtime import HttpBedrockAgentCoreClient, get_data_plane_endpoint
-
+import sys
 import yaml
-
+import boto3
 qualifier = "DEFAULT"
 # Configurable context window for chat history
 CONTEXT_WINDOW = 10  # Number of turns (user+assistant pairs) to include in context
+
+def get_streamlit_url():
+    try:
+        # Read the JSON file
+        with open('/opt/ml/metadata/resource-metadata.json', 'r') as file:
+            data = json.load(file)
+            domain_id = data['DomainId']
+            space_name = data['SpaceName']
+    except FileNotFoundError:
+        print("Resource-metadata.json file not found -- running outside SageMaker Studio")
+        domain_id = None
+        space_name = None
+        # sys.exit(1)
+    except json.JSONDecodeError:
+        print("Error: Invalid JSON format in resource-metadata.json")
+        sys.exit(1)
+    except KeyError as e:
+        print(f"Error: Required key {e} not found in JSON")
+        sys.exit(1)
+    
+    # Now you can use domain_id and space_name variables in your code
+    # print(f"Domain ID: {domain_id}")
+    # print(f"Space Name: {space_name}")
+    print(f"Please use the following to login and test the Streamlit Application")
+    print(f"Username:       testuser")
+    print(f"Password:       MyPassword123!")
+    if domain_id is not None:
+        sagemaker_client = boto3.client('sagemaker')
+        # Replace 'your-space-name' and 'your-domain-id' with your actual values
+        response = sagemaker_client.describe_space(
+            DomainId=domain_id,
+            SpaceName=space_name
+        )
+        
+        streamlit_url = response['Url']+"/proxy/8501/"
+    else:
+        streamlit_url = "http://localhost:8501"
+    return streamlit_url
+
 
 def build_context(messages, context_window=CONTEXT_WINDOW):
     # Only use the last context_window*2 messages (user+assistant pairs)
