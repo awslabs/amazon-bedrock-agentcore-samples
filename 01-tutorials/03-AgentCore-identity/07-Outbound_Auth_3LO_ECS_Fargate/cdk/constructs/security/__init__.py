ab9cc8f46e978a0737b0b67f3dc338e4af028bc2
Julian Grüber <julian.grueber@googlemail.com>
2026-02-24 20:20:56 +0000

    Add ECS Fargate 3LO tutorial
    Fixes #<issue-number>
    
    Co-authored-by: tnickl <tnickl@users.noreply.github.com>
    Co-authored-by: satveerkhurpa <satveerkhurpa@users.noreply.github.com>

---
+++ 01-tutorials/03-AgentCore-identity/07-Outbound_Auth_3LO_ECS_Fargate/cdk/constructs/security/__init__.py
@@ -0,0 +1,7 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+"""Security constructs package."""
+
+from .identity import Identity
+
+__all__ = ["Identity"]
